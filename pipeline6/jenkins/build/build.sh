b212187d64f45549611895299ab82e421c4bdbd1
canhnp <39284593+canhnp@users.noreply.github.com>
2020-08-26 15:18:17 +0700

    Update build.sh

--- pipeline6/jenkins/build/build.sh
+++ pipeline6/jenkins/build/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-cp -f java-app/target/*.jar jenkins/build/
+cp -f pipeline6/java-app/target/*.jar pipeline6/jenkins/build/
 
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build #--no-cache
+cd pipeline6/jenkins/build/ && docker-compose -f docker-compose-build.yml build #--no-cache
