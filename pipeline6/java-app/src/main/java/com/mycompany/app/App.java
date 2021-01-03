dc94300adff8328e1b7555fee2e77c0cb4e7b1cb
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-03 17:34:24 +0700

    Update App.java

--- pipeline6/java-app/src/main/java/com/mycompany/app/App.java
+++ pipeline6/java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from Pipeline! This is automation test.";
+    private final String message = "Hello from Pipeline!";
 
     public App() {}
 
