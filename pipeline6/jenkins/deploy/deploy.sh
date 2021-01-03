13f58b0d5a11877e7847e690f5167bcfa6f84a88
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-03 17:48:24 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp  /tmp/.auth root@192.168.1.50:/tmp/.auth
+scp /tmp/.auth root@192.168.1.50:/tmp/.auth
 scp ./pipeline6/jenkins/deploy/publish root@192.168.1.50:/tmp/publish
 ssh root@192.168.1.50 "/tmp/publish"
