04978cef33a5bcb21c7b69c673bf79850954b016
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2021-01-03 17:18:19 +0700

    Update build.sh

--- pipeline6/jenkins/build/build.sh
+++ pipeline6/jenkins/build/build.sh
@@ -7,4 +7,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd pipeline6/jenkins/build/ && docker-compose -f docker-compose-build.yml build #--no-cache
+cd pipeline6/jenkins/build/ && docker-compose -f docker-compose-build.yml build
